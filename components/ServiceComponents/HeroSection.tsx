1b83241f08e6a136a4e4f636246b0741e1a34e2e
DynamicAdi <adarshpanditdev@gmail.com>
2025-12-15 15:20:44 +0530

    fixes

--- components/ServiceComponents/HeroSection.tsx
+++ components/ServiceComponents/HeroSection.tsx
@@ -22,19 +22,13 @@ export default function HeroSection({isVideo = false, service, bgPic, titleColor
             )
       }
       <div className="md:max-w-5xl max-md:w-full mx-auto text-center">
-
-        {/* Small label */}
-        <p className={`text-xs tracking-[0.25em] ${titleColor==="black"?"text-black":"text-white"} mb-6`}>
-          SERVICES
-        </p>
-
         {/* Main heading */}
         <h1 className={`capitalize text-xl md:text-6xl font-serif ${titleColor==="black" ? "text-black":"text-white"} leading-tight`}>
           {service}
         </h1>
 
         {/* Subtext */}
-        <p className={`${titleColor==="black" ? "text-black":"text-white"} mt-6 text-sm md:text-base`}>
+        <p className={`${titleColor==="black" ? "text-black":"text-white"} mt-6 text-base md:text-2xl`}>
           Make Every Product Irresistible. Sell With Confidence.
         </p>
       </div>
