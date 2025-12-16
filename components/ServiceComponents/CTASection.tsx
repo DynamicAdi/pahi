461e1e060cc21d178711d6dc5f3bf4c36856a1f7
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2025-12-16 15:56:25 +0530

    Randwa hijra

--- components/ServiceComponents/CTASection.tsx
+++ components/ServiceComponents/CTASection.tsx
@@ -4,11 +4,11 @@ export default function CTASection() {
       <div className="md:max-w-7xl mx-auto bg-gray-100 dark:bg-neutral-900 rounded-2xl py-10 px-6 flex flex-col items-center text-center gap-6 transition-colors duration-300">
         
         <h2 className="text-black dark:text-white text-xl md:text-2xl font-light tracking-wide transition-colors duration-300">
-          Ready to elevate your e-commerce content?
+          Ready to elevate your brand's visual content??
         </h2>
 
         <a href="mailto:info@peritumproductions.com" className="bg-black dark:bg-white text-lg text-white dark:text-black font-medium tracking-wide px-6 py-3 rounded-md hover:bg-neutral-800 dark:hover:bg-neutral-200 transition">
-          Ready to elevate your brand's visual content.
+          Get a Quote
         </a>
 
       </div>
